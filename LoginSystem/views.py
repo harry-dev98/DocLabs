244aab1788cbd532568453430cfa50e20d114b14
Ajeet Singh <raba.ajeet@gmail.com>
2020-02-02 14:08:41 +0530

    user datas are added

--- LoginSystem/views.py
+++ LoginSystem/views.py
@@ -1,5 +1,27 @@
-from django.shortcuts import render
-
+from django.contrib.auth import authenticate, login, logout
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import render,redirect
+from django.urls import reverse
+from .models import UserData
 # Create your views here.
 def index(request):
-    return render(request,"base.html")
+    datas=UserData.objects.all()
+    return render(request,"index.html",{'datas':datas})
+
+
+def login_view(request):
+    if request.method=="POST":
+        username = request.POST["username"]
+        password = request.POST["password"]
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return HttpResponseRedirect(reverse("index"))
+        else:
+            return render(request, "login.html",{'message':"wrong credentials"}) 
+    else:
+        return render(request, "login.html")
+
+def logout_view(request):
+    logout(request)
+    return redirect('index')   
